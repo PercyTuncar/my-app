6264501be869009db5eca6bf7e0c44951983169f
Percy Tuncar <33427262+PercyTuncar@users.noreply.github.com>
2024-11-23 16:06:23 -0500

    Initial commit from Create Next App

---
+++ config/site.js
@@ -0,0 +1,62 @@
+export const siteConfig = {
+  name: "Next.js + NextUI",
+  description: "Realiza hermosos sitios web sin esfuerzo con NextUI.",
+  navItems: [
+    {
+      label: "Inicio",
+      href: "/",
+    },
+    {
+      label: "Precios",
+      href: "/pricing",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+   
+  ],
+  navMenuItems: [
+    {
+      label: "Inicio",
+      href: "/",
+    },
+    {
+      label: "Precios",
+      href: "/pricing",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+   
+    {
+      label: "Perfil",
+      href: "/profile",
+    },
+    {
+      label: "Proyectos",
+      href: "/projects",
+    },
+    {
+      label: "Calendario",
+      href: "/calendar",
+    },
+    {
+      label: "Configuración",
+      href: "/settings",
+    },
+    {
+      label: "Cerrar sesión",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/PercyTuncar",
+    twitter: "https://x.com/percy_tuncar",
+    docs: "https://nextui.org",
+    sponsor: "https://github.com/PercyTuncar",
+  },
+};
+
+export const SiteConfig = siteConfig;
